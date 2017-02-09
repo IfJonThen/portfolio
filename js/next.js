d6f3efbcb9228d2b2b2c12cade0593b5f86899cc
jonsenY <pikahatonjon@gmail.com>
2017-02-08 16:59:24 -0800

    edits made
    made some edits

--- js/next.js
+++ js/next.js
@@ -1,16 +1,15 @@
- var about1={"About":"Hello! Welcome to Jonathan's website. I am a Software Engineering student at the University of California. I enjoy learning about new software technologies and building projects. When not programming, I enjoy reading books. I am currently reading Game of Thrones: A Feast for Crows"
-        , "Skills": "I have experience using many web development technologies. To name a few, I have used Node.js, AngularJS for a few personal side projects, I am very familiar with the jQuery library"
-        ,"Goals":"I am currently finishing up my degree at UC Irvine but beyond that I would like to establish a web development career for myself. My current path is leading me to develop different projects to increase familiarity with technologies, however if you have advice or would like to know more, feel free to email me!"
-        };
-    function changeInfo(id){
-        switch(id){
-            case 2:
-                break;
-            case 3:
-                document.getElementById("about").innerHTML=about1['About'];
-                break;
-            case 4:
-                if (document.getElementById("content4").childElementCount==0){
+var about1={"About":"Hello! Welcome to Jonathan's website. I am a Software Engineering student at the University of California. I enjoy learning about new software technologies and building projects. When not programming, I enjoy reading books. I am currently reading Game of Thrones: A Feast for Crows",
+    "Skills": "I have experience using many web development technologies. I have lots of practical experience in building both personal websites as well as professional websites for multiple companies. I am skilled at designing dynamic webpages with different languages. I am extremely familiar with jQuery, NodeJS, React and AngularJS.",
+    "Goals":"I am currently finishing up my degree at UC Irvine but beyond that I would like to establish a web development career for myself. My current path is leading me to seek a position in Frontend development, however if you have advice or would like to chat, feel free to email me!"};
+function changeInfo(id){
+    switch(id){
+        case 2:
+            break;
+        case 3:
+            document.getElementById("about").innerHTML=about1['About'];
+            break;
+        case 4:
+            if (document.getElementById("content4").childElementCount==0){
                 var h = document.createElement("H1");
                 var t = document.createTextNode("Skills");
                 var p = document.createElement("P");
@@ -19,26 +18,26 @@
                 h.appendChild(t);
                 document.getElementById("content"+(id)).appendChild(h);
                 document.getElementById("content"+(id)).appendChild(p);
-                }
-                break;
-            case 5:
-                loadGoals();
-                break;
-            default:
-                break;
-        }
+            }
+            break;
+        case 5:
+            loadGoals();
+            break;
+        default:
+            break;
     }
-    function loadGoals(){
-        if (document.getElementById("content5").childElementCount==0){
-            let h = document.createElement("H1");
-            let t = document.createTextNode("Goals");
-            let p = document.createElement("P");
-            p.innerHTML=about1['Goals'];
-            t.id="goals";
-            h.appendChild(t);
-            document.getElementById("content5").appendChild(h);
-            document.getElementById("content5").appendChild(p);
-        }
+}
+function loadGoals(){
+    if (document.getElementById("content5").childElementCount==0){
+        let h = document.createElement("H1");
+        let t = document.createTextNode("Goals");
+        let p = document.createElement("P");
+        p.innerHTML=about1['Goals'];
+        t.id="goals";
+        h.appendChild(t);
+        document.getElementById("content5").appendChild(h);
+        document.getElementById("content5").appendChild(p);
     }
+}
 
 
